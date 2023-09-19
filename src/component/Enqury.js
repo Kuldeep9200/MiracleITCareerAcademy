b8e76cab6176135508abef5059dd52fa82e6e728
Kuldeep Mishra <108526029+Kuldeep9200@users.noreply.github.com>
2023-09-19 13:25:47 +0530

    Update Enqury.js

--- src/component/Enqury.js
+++ src/component/Enqury.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { cloneElement } from 'react';
+import { cloneElement } from "react";
 import { Modal, Button } from "react-bootstrap";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
